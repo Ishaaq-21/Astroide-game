5d35fbb50e30f6af94499e39f8d0cff740eedf46
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-01-09 09:16:31 +0100

    use the new player class logic to draw a triangle in the screen

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 def main():
+
     pygame.init()
+    print("Starting Asteroids")
+    print("Screen width: 1280")
+    print("Screen height: 720")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -11,7 +20,8 @@ def main():
                 return
         log_state()
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
