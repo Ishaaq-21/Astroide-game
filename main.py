4723e5520fffd9f5bc5bcc9068ece0dc35de9007
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-01-09 10:27:37 +0100

    rafactor: arrange our objects using pygame groupe

--- main.py
+++ main.py
@@ -12,16 +12,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    updatable  = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
-        player.update(dt)
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
