724bc270de927e5cdd202cc4706905045f927d72
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-01-13 21:13:04 +0100

    add the logic of ending the game when the player collides with an asteroid

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
+import sys
 def main():
 
     pygame.init()
@@ -15,9 +19,13 @@ def main():
 
     updatable  = pygame.sprite.Group()
     drawable  = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
+    astroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
@@ -26,6 +34,11 @@ def main():
         log_state()
 
         updatable.update(dt)
+        for obj in asteroids :
+            if obj.collides_with(player) :
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
