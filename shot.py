f1d3ddadfb61519916d491294b10379fa1ecaf6c
Ishaaq-21 <ishak.hadj14@gmail.com>
2026-01-14 09:35:35 +0100

    feat: improve the shots shape

--- shot.py
+++ shot.py
@@ -6,7 +6,16 @@ class Shot(CircleShape) :
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        # Draw a directional streak
+        if self.velocity.length() > 0:
+            # Calculate tail and head
+            # Visual length can be longer than hitbox radius
+            direction = self.velocity.normalize()
+            head = self.position + direction * self.radius
+            tail = self.position - direction * self.radius * 2
+            pygame.draw.line(screen, "white", tail, head, LINE_WIDTH + 1)
+        else:
+            pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
 
 
 
